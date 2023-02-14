93a7ab8dad5b381b756a3f013b4a7eed4ecea08c
Séamus Hirrell <seamushirrell@gmail.com>
2023-02-14 15:59:59 +0000

    remove annoying comments

--- practical_2/src/practical_2/Practical_2.java
+++ practical_2/src/practical_2/Practical_2.java
@@ -14,7 +14,6 @@ public class Practical_2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.println("Hello From Magee!");
     }
     
